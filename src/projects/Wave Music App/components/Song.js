15539c56dfce2bca01f1d88c0a4e7eb51507bdff
JacobMarsay <jmmarsay@outlook.com>
2023-02-19 19:09:50 +0000

    Added base components and installed font awesome

---
+++ src/projects/Wave Music App/components/Song.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Song = () => {
+  return (
+    <div className="song__container">
+      {/* <img alt={currentSong.name} src={currentSong.cover}></img> */}
+      <h2>song name</h2>
+      <h3>artist</h3>
+    </div>
+  );
+};
+
+export default Song;
