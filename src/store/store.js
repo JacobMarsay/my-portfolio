4a376a4b777a80881aca081b8a09194b73dc9045
JacobMarsay <jmmarsay@outlook.com>
2023-02-19 18:38:52 +0000

    store and music slice created

---
+++ src/store/store.js
@@ -0,0 +1,5 @@
+import { configureStore } from "@reduxjs/toolkit";
+import musicSlice from "../projects/Wave Music App/redux/slices/musicSlice";
+export const store = configureStore({
+  reducer: {},
+});
