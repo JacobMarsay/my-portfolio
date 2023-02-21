43874d483c0d868ab8b8cbedae298c319dcd3af4
JacobMarsay <jmmarsay@outlook.com>
2023-02-21 01:13:20 +0000

    Fixed typo and added library toggle functionality

--- src/projects/Wave Music App/redux/slices/musicSlice.js
+++ src/projects/Wave Music App/redux/slices/musicSlice.js
@@ -1,7 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 import songData from "../../util/songData";
 
-const intialState = {
+const initialState = {
   songs: songData(),
   currentSong: null,
   isPlaying: false,
@@ -12,16 +12,21 @@ const intialState = {
 };
 
 export const playerSlice = createSlice({
-  name: "music player",
-  intialState,
+  name: "player",
+  initialState,
   reducers: {
     activeSong: (state) => {},
     playlist: (state) => {},
     songInfo: (state) => {},
     musiclibrary: (state) => {},
+
+    toggleLibrary: (state) => {
+      state.libraryStatus = !state.libraryStatus;
+      console.log(state.libraryStatus);
+    },
   },
 });
 
-export const { activeSong, playlist } = playerSlice.actions;
+export const { activeSong, playlist, toggleLibrary } = playerSlice.actions;
 
 export default playerSlice.reducer;
