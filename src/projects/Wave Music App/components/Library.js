59cba89c5ac12424048f87960ecc03e1f170faf3
JacobMarsay <jmmarsay@outlook.com>
2023-02-21 01:30:30 +0000

    Conditional rendering the library on toggle

--- src/projects/Wave Music App/components/Library.js
+++ src/projects/Wave Music App/components/Library.js
@@ -1,15 +1,25 @@
 import React from "react";
+import styled from "styled-components";
+//import redux
+import { useSelector } from "react-redux";
 import LibrarySongs from "./LibrarySongs";
 
 const Library = () => {
+  const toggle = useSelector((state) => state.player.libraryStatus);
+
   return (
-    // END OF LINE 14 - IF THE LIBRARY STATUS IS ACTIVE, SHOW 'active__library'. OTHERWISE SHOW NOTHING
-    <div>
-      <h2>Library</h2>
-      <div className="library__songs">
-        <LibrarySongs />
-      </div>
-    </div>
+    <>
+      {toggle ? (
+        <div>
+          <h2>Library</h2>
+          <div className="library__songs">
+            <LibrarySongs />
+          </div>
+        </div>
+      ) : (
+        ""
+      )}
+    </>
   );
 };
 export default Library;
