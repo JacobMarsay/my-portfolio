a36d05aef6143d3e365e97f7303bf436aa6b8388
Jacob Marsay <jacobmarsay@calls9.com>
2023-03-22 16:22:26 +0000

    Custom hook to get Vertical viewport values

---
+++ src/hooks/scrollY.js
@@ -0,0 +1,19 @@
+// hooks/useScroll.js
+import { useState, useEffect } from 'react';
+
+export const useScroll = () => {
+  const [scrollY, setScrollY] = useState(0);
+
+  const handleScroll = () => {
+    setScrollY(window.scrollY);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  return scrollY;
+};
