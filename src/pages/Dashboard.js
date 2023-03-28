cd0253d001d91b8e71dc2364cc3f2fe96bd39258
JacobMarsay <jmmarsay@outlook.com>
2023-03-28 22:53:02 +0100

    Code clean up and hero image styles fix

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,10 +1,10 @@
 import { React, useState, useEffect } from "react";
 import styled from "styled-components";
-import { 
-  motion, 
+import {
+  motion,
   useAnimation,
   AnimateSharedLayout,
-  useViewportScroll
+  useViewportScroll,
 } from "framer-motion";
 
 //Import components
@@ -16,23 +16,26 @@ import TypedText from "../anims/TypedText";
 import CurvedLine from "../components/CurvedLine";
 
 //import global styles
-import { PageContainer, PageContentContainer, SectionHeadingContainer} from "../styles/global/Pages";
+import {
+  PageContainer,
+  PageContentContainer,
+  SectionHeadingContainer,
+} from "../styles/global/Pages";
 import { ScrollyHeaderWrapper } from "../styles/sections/SideBySide";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { ProjectData } from "../util/project-util";
-import { faGithub , faLinkedin} from '@fortawesome/free-brands-svg-icons';
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import { scaleSection } from "../anims/animations";
-import useScroll from '../hooks/useScroll';
+import useScroll from "../hooks/useScroll";
 import { useParallax } from "../hooks/useParallax";
 import AboutCta from "../components/AboutCta";
 
 const Dashboard = () => {
   const [element, controls] = useScroll();
 
-
   const leftOffset = useParallax(0.2, "left");
   const rightOffset = useParallax(0.1, "right");
-  
+
   const projectInfo = ProjectData();
   const heroAnimation = useAnimation();
 
@@ -41,8 +44,8 @@ const Dashboard = () => {
       opacity: 1,
       transition: {
         duration: 0.5,
-        ease: "easeInOut"
-      }
+        ease: "easeInOut",
+      },
     });
   };
 
@@ -51,7 +54,7 @@ const Dashboard = () => {
     <PageContainer>
       <Nav />
       <HeroContainer>
-        <HeroImage heroImage={Hero}/>
+        <HeroImage heroImage={Hero} />
         <HeroOverlay animate={heroAnimation}>
           <HeroHeadingContainer>
             <motion.h1
@@ -60,44 +63,50 @@ const Dashboard = () => {
               exit={{ opacity: 0, y: 10 }}
               transition={{ duration: 2 }}
             >
-              <TypedText text="A Fullstack Developer with Passion For Modern Web Apps and Design." 
-              />
+              <TypedText text="A Fullstack Developer with Passion For Modern Web Apps and Design." />
             </motion.h1>
             <UnderlineContainer>
-              <CurvedLine/>
+              <CurvedLine />
             </UnderlineContainer>
           </HeroHeadingContainer>
         </HeroOverlay>
         <HeroIconsContainer>
-          <FontAwesomeIcon icon={faGithub} size="5x" className="icon github"/>
-          <FontAwesomeIcon icon={faLinkedin} size="5x" className="icon linkedin"/>
+          <FontAwesomeIcon icon={faGithub} size="5x" className="icon github" />
+          <FontAwesomeIcon
+            icon={faLinkedin}
+            size="5x"
+            className="icon linkedin"
+          />
         </HeroIconsContainer>
       </HeroContainer>
       <PageContentContainer>
         <SectionHeadingContainer>
-          <ScrollyHeaderWrapper style={{ right: rightOffset }} direction="right">
+          <ScrollyHeaderWrapper
+            style={{ right: rightOffset }}
+            direction="right"
+          >
             <motion.span>01</motion.span>
             <motion.h2>My Projects</motion.h2>
           </ScrollyHeaderWrapper>
         </SectionHeadingContainer>
-          <CardsContainer
-            variants={scaleSection}
-            animate={controls}
-            ref={element}
-            initial="hidden"
-          >
-            <AnimateSharedLayout>
-              {projectInfo.map((project) => (
-                <Card
-                  key={project.id}
-                  id={project.id}
-                  image={project.image.CardPlaceHolderImg}
-                  title={project.title}
-                  description={project.description}
-                />
-              ))}
-            </AnimateSharedLayout>
-          </CardsContainer>
+        <CardsContainer
+          variants={scaleSection}
+          animate={controls}
+          ref={element}
+          initial="hidden"
+        >
+          <AnimateSharedLayout>
+            {projectInfo.map((project) => (
+              <Card
+                key={project.id}
+                id={project.id}
+                image={project.image.CardPlaceHolderImg}
+                title={project.title}
+                description={project.description}
+              />
+            ))}
+          </AnimateSharedLayout>
+        </CardsContainer>
         <AboutCta />
       </PageContentContainer>
     </PageContainer>
@@ -115,6 +124,7 @@ export const CardsContainer = styled(motion.div)`
 
 export const HeroContainer = styled(motion.div)`
   position: relative;
+  width: 100%;
 `;
 
 export const HeroHeadingContainer = styled(motion.div)`
@@ -126,12 +136,12 @@ export const HeroHeadingContainer = styled(motion.div)`
   align-items: center;
   justify-content: flex-start;
   color: white;
-  h1{
-    font-family: 'Poppins';
+  h1 {
+    font-family: "Poppins";
     font-size: 8rem;
     line-height: 8rem;
   }
-`
+`;
 
 export const ScrollWrapper = styled(motion.div)`
   position: absolute;
@@ -143,29 +153,29 @@ export const ScrollWrapper = styled(motion.div)`
   z-index: 1;
 `;
 
-export const HeroIconsContainer = styled.div `
-    position: absolute;
-    bottom: 0;
-    left: 8%;
-    display: flex;
-    padding: 20px;
-    gap: 20px;
+export const HeroIconsContainer = styled.div`
+  position: absolute;
+  bottom: 0;
+  left: 8%;
+  display: flex;
+  padding: 20px;
+  gap: 20px;
 
-    .icon {
-      position: relative;
-      z-index: 3;
-      color: white;
-      padding: 10px;
-    }
-  
-    .github {
-      left: 0;
-    }
-  
-    .linkedin {
-      left: 5px;
-    }
-`
+  .icon {
+    position: relative;
+    z-index: 3;
+    color: white;
+    padding: 10px;
+  }
+
+  .github {
+    left: 0;
+  }
+
+  .linkedin {
+    left: 5px;
+  }
+`;
 export const HeroOverlay = styled(motion.div)`
   position: absolute;
   top: 0;
@@ -183,9 +193,8 @@ export const HeroOverlay = styled(motion.div)`
   );
 `;
 
-export const UnderlineContainer = styled.div `
+export const UnderlineContainer = styled.div`
   position: absolute;
   bottom: -400px;
   left: 0px;
-`
-
+`;
--- src/styles/global/Pages.js
+++ src/styles/global/Pages.js
@@ -4,10 +4,8 @@ import { motion } from "framer-motion";
 export const PageContainer = styled.div`
   display: flex;
   flex-wrap: wrap;
-  /* justify-content: center; */
-  width: 100%;
+  max-width: 100vw;
   height: 100vh;
-  min-width: 100vw;
 `;
 
 export const PageContentContainer = styled.div`
@@ -15,17 +13,17 @@ export const PageContentContainer = styled.div`
 `;
 
 export const SectionHeadingContainer = styled.div`
- position: relative;
- width: 100%;
- height: 40vh;
- overflow-x: hidden;
-`
-export const ButtonLayoutElement = styled(motion.button) `
+  position: relative;
+  width: 100%;
+  height: 40vh;
+  overflow-x: hidden;
+`;
+export const ButtonLayoutElement = styled(motion.button)`
   padding: 1.5rem 5rem;
   border-radius: 1rem;
   font-size: 1.5rem;
-  font-family: 'Poppins';
+  font-family: "Poppins";
   background-color: transparent;
   border-color: white;
   color: white;
-`
+`;
