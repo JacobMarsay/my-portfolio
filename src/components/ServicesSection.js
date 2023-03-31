db317b7acde47be47cd78d4e4d674f9d7f81fd11
JacobMarsay <jmmarsay@outlook.com>
2023-03-31 11:31:21 +0100

    Clean up

--- src/components/ServicesSection.js
+++ src/components/ServicesSection.js
@@ -4,7 +4,6 @@ import {
   ScrollyHeaderWrapper,
 } from "../styles/sections/SideBySide";
 import { useParallax } from "../hooks/useParallax";
-import useScroll from "../hooks/useScroll";
 import { AnimateSharedLayout, motion } from "framer-motion";
 import styled from "styled-components";
 import {
